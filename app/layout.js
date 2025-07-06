9728a975ea717c9cf1b2f1de9e84799b87b17682
gourabop3 <gourabmullick200@gmail.com>
2025-07-06 14:15:48 +0530

    Fix login functionality issue (#4)

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import AuthContextProvider from "@/contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -28,7 +29,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster />
-        <NextUIProvider>{children}</NextUIProvider>
+        <AuthContextProvider>
+          <NextUIProvider>{children}</NextUIProvider>
+        </AuthContextProvider>
       </body>
     </html>
   );
